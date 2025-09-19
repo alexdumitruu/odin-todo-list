54e4f5f0133b82dc9baff6b8a9825997bf4a1eb1
Alexandru David Dumitru <alex.dumitru.g@gmail.com>
2025-09-19 15:43:21 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "distMain.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"], //css
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader", //images in html
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg)$/i,
+        type: "asset/resource", //images in js
+      },
+    ],
+  },
+};

70cab7ae7e097c9c04c1c12c648655f7c41e54a0
alexdumitruu <alex.dumitru.g@gmail.com>
2025-09-22 15:52:33 +0300

    render page

--- src/storage.js
+++ src/storage.js
@@ -2,7 +2,7 @@ const storageModule = (function() {
     const saveState = (projectsArr) => {
         projectsArr.forEach(project => {
             const objJSON = JSON.stringify(project);
-            const key = `project_${project.getId()}`; // safer key
+            const key = `project_${project.getId()}`;
             localStorage.setItem(key, objJSON);
         });
     }
@@ -13,6 +13,11 @@ const storageModule = (function() {
             const key = localStorage.key(i);
             if (key.startsWith('project_')) {
                 const data = JSON.parse(localStorage.getItem(key));
+                const restoredTodos = data.todos.map(t => new ToDo(
+                t.id, t.title, t.description, t.dueDate,
+                t.priority, t.checklist, t.completed
+            ));
+                const restoredProject = new Project(data.id, data.name, restoredTodos);
                 projects.push(data);
             }
         }
--- src/ui.js
+++ src/ui.js
@@ -1,2 +1,17 @@
+import { storageModule } from "./storage";
+
 const app = document.querySelector('.app');
 
+const renderModule = (function() {
+    const renderPage = () => {
+        let sidebar = document.createElement('div');
+        sidebar.classList.add('sidebar')
+        let projects = storageModule.loadState();
+        projects.forEach(p => {
+            let projDiv = document.createElement('div');
+            projDiv.classList.add('project')
+            projDiv.textContent = p.getName();
+            sidebar.appendChild(projDiv);
+        });
+    }
+})();

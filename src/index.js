3d6638645b8cba9b197c1b15066f55df62a85bbe
alexdumitruu <alex.dumitru.g@gmail.com>
2025-09-23 11:14:22 +0300

    progress on rendering ui

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import { ToDo, Project } from "./classes";
 import { controllerModule } from "./appLogic";
+import "./styles.css";
+import { renderModule } from './ui.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderModule.renderPage();
+});
--- src/ui.js
+++ src/ui.js
@@ -53,7 +53,7 @@ const renderModule = (function() {
         });
         // todos
         let list = document.createElement('div');
-        list.classList.add('list')
+        list.classList.add('list');
 
         projects.forEach(p => {
             let projDiv = document.createElement('div');
@@ -62,16 +62,69 @@ const renderModule = (function() {
             sidebar.appendChild(projDiv);
 
             projDiv.addEventListener("click", () => {
+                let newToDoDiv = document.createElement('div');
+                newToDoDiv.classList.add('new-todo-container');
+                list.appendChild(newToDoDiv);
+
+                const input = document.createElement('input');
+                input.classList.add('input-todo');
+
+                const dueDateInput = document.createElement('input');
+                dueDateInput.classList.add('input-date-todo');
+                dueDateInput.setAttribute('type', 'date');
+
+                const comboBoxPriority = document.createElement('select');
+                let option1 = document.createElement('option');
+                let option2 = document.createElement('option');
+                let option3 = document.createElement('option');
+                option1.textContent = 'Low';
+                option2.textContent = 'Medium';
+                option3.textContent = 'High';
+                comboBoxPriority.appendChild(option1);
+                comboBoxPriority.appendChild(option2);
+                comboBoxPriority.appendChild(option3);
+
+                const descriptionTextBox = document.createElement('textarea');
+                descriptionTextBox.classList.add('textarea-todo');
+
+                const addTaskBtn = document.createElement('button');
+                const cancelTaskBtn = document.createElement('button');
+
+                //to add button functionalities
+
                 let todoContainer = document.createElement('div');
                 todoContainer.classList.add('todo-container');
                 p.getTodos().forEach(t => {
                     let todoDiv = document.createElement('div');
                     todoDiv.classList.add('todo');
                     
+                    let titleElement = document.createElement('h2');
+                    let dueDateElement = document.createElement('p');
+                    let moreInfoBtn = document.createElement('button');
+
+                    titleElement.classList.add('todo-title');
+                    dueDateElement.classList.add('todo-duedate');
+                    moreInfoBtn.classList.add('todo-moreinfo');
+
+                    titleElement.textContent = t.getTitle();
+                    dueDateElement.textContent = t.getDueDate();
+                    moreInfoBtn.textContent = 'Expand';
+
+                    todoDiv.appendChild(titleElement);
+                    todoDiv.appendChild(dueDateElement);
+                    todoDiv.appendChild(moreInfoBtn);
+
+                    moreInfoBtn.addEventListener('click', () => {
+                        todoDiv.classList.remove('todo');
+                        todoDiv.classList.add('todo-expanded');
+                        // to add more info of todo
+                        todoDiv.append()
+                    })
                 })
             })
         });
-
-
+        return { renderPage };
     }
 })();
+
+export {renderModule};
